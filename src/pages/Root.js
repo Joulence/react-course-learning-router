8035a0ce1cc5e791a04fd279dfc6cc37e372f82a
Joulence <4erviken@gmail.com>
2023-05-27 15:33:50 +0300

    Showing Error Pages with errorElement

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,14 +2,12 @@ import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
-import classes from "./Root.module.css";
-
 function RootLayout() {
     return (
         <Fragment>
             <MainNavigation />
-            <main className={classes.content}>
-            <Outlet />
+            <main>
+                <Outlet />
             </main>
         </Fragment>
     );
