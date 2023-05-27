444a40d5e3563c071fdb52a4c37ade532860dbfc
Joulence <4erviken@gmail.com>
2023-05-27 14:38:53 +0300

    Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+
+createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+]);
+
 function App() {
   return <div></div>;
 }
